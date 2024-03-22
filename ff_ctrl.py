55470859b35fc6909937450411d8d02dd1f62f2a
aihoque2 <aihoque2@illinois.edu>
2024-03-21 17:12:17 -0700

    milestone 3 done...7 hours left for part 4 fuck

--- ff_ctrl.py
+++ ff_ctrl.py
@@ -3,7 +3,43 @@ import numpy.linalg as nla
 import modern_robotics as mr
 
 
-def FeedbackControl(Tse:np.ndarray, Tse_d:np.ndarray, Tse_dnext:np.ndarray, kp: float, ki: float):
+# T matrix from chassis frame to b to arm frame O
+Tb0 = np.array([[1,0,0,0.1662],
+                    [0,1,0,0],
+                    [0,0,1,0.0026],
+                    [0,0,0,1]])
+
+# M matrix of end-effector in arm frame coords
+M = np.array([[1,0,0,0.033],
+                    [0,1,0,0],
+                    [0,0,1,0.6546],
+                    [0,0,0,1]])
+
+# screw axes
+B1 = np.array([0, 0, 1, 0, 0.033, 0])
+B2 = np.array([0, -1, 0, -0.5076, 0, 0])
+B3 = np.array([0,-1, 0, -0.3526, 0, 0]) 
+B4 = np.array([0, -1, 0, -0.2176, 0, 0])
+B5 = np.array([0, 0, 1, 0, 0, 0])
+
+B = np.array([B1, B2, B3, B4, B5])
+
+def joint_limits(positions):
+    arm_thetas = positions[1]
+
+    joint_free = np.ones(arm_thetas.shape, dtype=bool)
+
+    if arm_thetas[2] > -0.1:
+        joint_free[2] = False
+    
+    if arm_thetas[3] > -0.1:
+        joint_free[3] = False
+
+    return joint_free        
+
+    
+
+def FeedbackControl(Tse:np.ndarray, Tse_d:np.ndarray, Tse_dnext:np.ndarray, kp: float, ki: float, positions, test_joints, errors):
     """
     feedforward+feedback control law as discussed in class.
     
@@ -15,33 +51,144 @@ def FeedbackControl(Tse:np.ndarray, Tse_d:np.ndarray, Tse_dnext:np.ndarray, kp: 
     kp: scalar of proportional gain
 
     """
+    r = 0.0475
+    l = 0.235
+    w = 0.15
     dt = 0.01
 
     Xe = Tse
     Xd = Tse_d
     Xd_next = Tse_dnext
 
-    kp = 0.045
-    ki = 0.35
+
     Kp = kp * np.eye(4)
     Ki = ki* np.eye(4)
  
-    Xd_inv = nla.inv(Xd)
-    Xe_inv = nla.inv(Xe)
+    Xd_inv = mr.TransInv(Xd)
+    Xe_inv = mr.TransInv(Xe)
 
     Vd_mat = mr.MatrixLog6((Xd_inv @ Xd_next)/dt)
     Vd = mr.se3ToVec(Vd_mat)
-    Verr_mat = mr.MatrixLog6(Xe_inv @ Xd)
-    Adj = mr.Adjoint(Xe_inv @ Xd)
-    V_new =  np.dot(Adj, Vd) + Kp@Xe + Ki
-
-
-    # screw axes
-    B1 = np.array([0, 0, 1, 0, 0.033, 0])
-    B2 = np.array([0, -1, 0, -0.5076, 0, 0])
-    B3 = np.array([0,-1,0,-0.3526, 0, 0]) 
-    B4 = np.array([0, -1, 0, -0.2176, 0, 0])
-    B5 = np.array([0, 0, 1, 0, 0, 0])
-
-    Jb_0 = np.array([B1, B2, B3, B4, B5])
     
+    Xerr = mr.se3ToVec(mr.MatrixLog6(Xe_inv @ Xd))
+
+    """
+    TODO: calculate error sum
+    """
+
+    Adj = mr.Adjoint(Xe_inv @ Xd)
+    print("adjoint shape: ", Vd.shape)
+    errors += (mr.Adjoint(Ki)@Xerr)*dt
+    V_new =  Adj@Vd + mr.se3ToVec(Kp@Xe) + errors
+    print("result of adj@Vd: ", Adj@Vd)
+
+    """
+    CALC'ing JACOBIAN
+    """
+
+    H_0 = (1/r)*np.array([[-l-w,1,-1],
+                  [l+w,1,1],
+                  [l+w,1,-1],
+                  [-l-w,1,1]])
+
+    F6 = np.zeros([6,4])
+    F = np.linalg.pinv(H_0)
+    F6[2:5,:] = F
+
+    theta_arm = positions[1]
+     # EE config in {0} coords
+    T0e = mr.FKinBody(M, B.T, theta_arm)
+
+    Teb = mr.TransInv(T0e) @ mr.TransInv(Tb0)
+
+    J_base = mr.Adjoint(Teb) @ F6
+    J_arm = mr.JacobianBody(B.T, theta_arm)
+
+    # end effector jacobian
+    Je = np.concatenate((J_base, J_arm), axis=1)
+    Je[np.abs(Je) < 1e-6] = 0
+
+    #Je[:, np.where(test_joints == False)] = 0
+
+    u_thetas = nla.pinv(Je)@V_new
+    u_thetas[np.abs(u_thetas) < 1e-9] = 0.0
+    return u_thetas, Xerr, errors, Je, V_new, Vd    
+
+
+def get_Tse(positions, T0e, Tb0):
+    
+    phi = positions[0]
+    x = positions[1]
+    y = positions[2]
+    z = 0.0963 # chassis height
+
+    Tsb = np.array([[np.cos(phi),-np.sin(phi),0,x],
+                    [np.sin(phi),np.cos(phi),0,y],
+                    [0,0,1,z],
+                    [0,0,0,1]])
+
+    Teb = mr.TransInv(T0e) @ mr.TransInv(Tb0)
+
+    Tse = Tsb @ mr.TransInv(Teb)
+    return Tse
+
+
+
+""""
+testing
+"""
+chassis_config = np.array([0.0,0.0,0.0])
+arm_thetas = np.array([0.0,0.0,0.2,-1.6,0.0])
+wheel_thetas = np.array([0.0,0.0,0.0,0.0])
+position_state = [chassis_config,arm_thetas,wheel_thetas]
+
+test_joints = joint_limits(position_state)
+
+
+
+# Fixed Offset between the end effector and the base of the arm frameb calculated from trajectory generation
+
+# EE config in {0} coords
+T0e = mr.FKinBody(M, B.T, arm_thetas.T)
+
+# Testing the functions
+Tse = get_Tse(position_state[0], T0e, Tb0)
+
+
+# Feedback control law
+Xd = np.array([[0,0,1,0.5],
+                [0,1,0,0],
+                [-1,0,0,0.5],
+                [0,0,0,1]])
+
+Xd_next = np.array([[0,0,1,0.6],
+                    [0,1,0,0],
+                    [-1,0,0,0.3],
+                    [0,0,0,1]])
+    
+#kp = 0.045
+#ki = 0.35
+kp = 0.0
+ki = 0.0
+
+Ki_error = 0
+
+dt = 0.01
+
+u_theta, Xerr, Ki_error, J_e, V_t, Vd = FeedbackControl(Tse, Xd, Xd_next, kp, ki, position_state, test_joints, Ki_error)
+
+print("++++++++++++++++++++++++++++++")
+print("Vd")
+print(Vd)
+print("++++++++++++++++++++++++++++++")
+print("Xerr")
+print(Xerr)
+print("++++++++++++++++++++++++++++++")
+print("Je")
+print(J_e)
+print("++++++++++++++++++++++++++++++")
+print("V_t")
+print(V_t)
+print("++++++++++++++++++++++++++++++")
+print("U_theta")
+print(u_theta)
